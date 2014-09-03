9a754eaee80fc1ad90ef83abc408a1c2304af299
Krzysztof Langner <klangner@gmail.com>
2014-09-03 14:06:27 +0200

    Wroking camera demo

--- camera/main.py
+++ camera/main.py
@@ -1,36 +1,33 @@
-'''
-Created on 2014-09-01
-
-@author: Krzysztof Langner
-'''
-
+import picamera
 from mewa.client import Connection
-import base64    
+import base64
 
 URI_IMAGE_SERVICE = "org.fi24.image"
 URI_IMAGE_GETIMAGE = "org.fi24.image.GetImage"
 URI_IMAGE_IMAGE = "org.fi24.image.Image"
 
 
-#connection = Connection("ws://mewa.cc/ws")
-connection = Connection("ws://localhost:9000/ws")
+connection = Connection("ws://channels.followit24.com/ws")
+camera = picamera.PiCamera()
+camera.resolution = (800, 600)
+
 
 def onMessage(timestamp, fromDevice, msgId, params):
     if msgId == URI_IMAGE_GETIMAGE:
-        with open("owl.jpg", "rb") as image_file:
+        camera.capture("image.jpg")
+        with open("image.jpg", "rb") as image_file:
             image = "data:image/png;base64," + base64.b64encode(image_file.read())
             connection.sendMessage(fromDevice, URI_IMAGE_IMAGE, {"data":image})
     elif msgId == "org.fi24.discovery.GetServices":
         connection.sendMessage(fromDevice, "org.fi24.discovery.ServiceList", ["org.fi24.discovery", URI_IMAGE_SERVICE])
-    
-    
+
+
 def onError(reason):
     print("Error: " + reason)
-    
+
 
 if __name__ == "__main__":
     connection.onMessage = onMessage
     connection.onError = onError
-    connection.connect("test", "camera", "test")
-
+    connection.connect("channel", "camera", "password")
 
